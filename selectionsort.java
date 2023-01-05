72423742075da16dd2a141f4982a13e891a9ef1c
Ragula Karthik <95897152+Ragulakarthik@users.noreply.github.com>
2023-01-05 15:45:38 +0000

    first commit

---
+++ selectionsort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+class selectionsort
+{
+	public static void swap(int a[],int b,int c)
+	{
+		int temp=a[b];
+		a[b]=a[c];
+		a[c]=temp;
+	}
+	public static void sort(int a[],int l)
+	{
+		for(int i=0;i<l;i++)
+		{
+			int min=i;
+			for(int j=i+1;j<l;j++)
+			{
+				if(a[min]>a[j])
+				{
+					min=j;
+				}
+			}
+			swap(a,i,min);
+		}
+		for(int i=0;i<l;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+	public static void main(String args[])
+	{
+		int a[]={7,4,10,8,3,1};
+		int l=a.length;
+		sort(a,l);
+	}
+}
